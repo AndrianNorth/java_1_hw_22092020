de4db173475b17fb3b5d4caad8427e692f959a10
AndrianNorth <disturbed7112@gmail.com>
2020-10-04 01:11:32 +0700

    hw 3

--- src/lesson3/HW3.java
+++ src/lesson3/HW3.java
@@ -8,23 +8,35 @@ public class HW3 {
     static Random random = new Random();
 
     public static void main(String[] args) {
-        int answer = random.nextInt(9);
-        int tryCount = 0;
+        int max = 10;
         int userAnswer;
         do {
-            System.out.println("Введите число от 0 д 9. У вас есть 3 попытки. ");
-            System.out.println(answer);
+            int answer = random.nextInt(max);
+            boolean guess = false;
+            System.out.printf("Введите число от 0 до %d. У вас есть 3 попытки.", max - 1);
+            System.out.println();
+            for (int i = 0; i < 3; i++) {
+                userAnswer = sc.nextInt();
+                if (userAnswer > answer) {
+                    System.out.println("Загаданное число меньше");
+                }
+                if (userAnswer < answer) {
+                    System.out.println("Загаданное число больше");
+                }
+                if (userAnswer == answer) {
+                    guess = true;
+                    break;
+                }
+            }
+            if (guess) {
+                System.out.println("Вы угадали!");
+            } else {
+                System.out.println("Вы неугадали! Ответ: " + answer);
+            }
+            System.out.println("Повторить игру еще раз? 1 – да / 0 – нет");
             userAnswer = sc.nextInt();
-            tryCount++;
-            if (userAnswer > answer) {
-                System.out.println("Загаданное число меньше");
-            } else if (userAnswer < answer) {
-                System.out.println("Загаданное число больше");
-            } else tryCount = 3;
-        } while (tryCount < 3);
-        System.out.println("Повторить игру еще раз? 1 – да / 0 – нет");
-        userAnswer = sc.nextInt();
-        if (userAnswer > 0) tryCount = 0;
+        } while (userAnswer == 1);
+
     }
 
 
